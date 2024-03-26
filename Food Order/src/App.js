60279135f722048019c0a754cde2f10f492059f4
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2024-03-26 21:59:14 +0530

    Update App.js

--- Food Order/src/App.js
+++ Food Order/src/App.js
@@ -1,6 +1,5 @@
-
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Component/Login';
 import Signup from './Component/Signup';
 import Landingpage from './Component/Landingpage';
@@ -10,23 +9,23 @@ import Terms from './Component/Terms';
 import Aboutus from './Component/Aboutus';
 import Contactus from './Component/Contactus';
 
-function App()
-{
-  return(
+function App() {
+  return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Firstpage/>}/>  
-        <Route path='/Login' element={<Login/>}/>
-        <Route path='/Privacy' element={<Privacy/>}/>
-        <Route path='/Signup' element={<Signup/>}/>
-        <Route path='/Terms' element={<Terms/>}/>
-        <Route path='/Aboutus' element={<Aboutus/>}/>
-        <Route path='/Landingpage' element={<Landingpage/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Firstpage />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Privacy' element={<Privacy />} />
+          <Route path='/Signup' element={<Signup />} />
+          <Route path='/Terms' element={<Terms />} />
+          <Route path='/Aboutus' element={<Aboutus />} />
+          <Route path='/Contactus' element={<Contactus />} />
+          <Route path='/Landingpage' element={<Landingpage />} />
+        </Routes>
       </BrowserRouter>
-
     </div>
   );
 }
+
 export default App;
